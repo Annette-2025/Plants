e43e503d3e6991c8ffa6c4e88f521c005408f9ff
Annette-2025 <limogesa@outlook.com>
2025-05-09 03:20:08 -0400

    tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const { Builder, By, until } = require('selenium-webdriver');
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-        await driver.get('https://example.com');
+        await driver.get('http://localhost:4444/wd/hub');
         let title = await driver.getTitle();
         console.log("Page title is:", title);
     } finally {
