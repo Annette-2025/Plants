c1f1e99061a4e21429175e40dab7bf1bf4b6f9cc
Annette-2025 <limogesa@outlook.com>
2025-05-09 02:45:21 -0400

    tests

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const assert = require("assert");
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-        await driver.get('https://github.com/Annette-2025/Plants');
+        await driver.get('https://localhost:8080');
         let title = await driver.getTitle();
         console.log("Page title is:", title);
     } finally {
