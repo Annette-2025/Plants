480230d6ad3c0975834c360b6230304a96158c6f
Annette-2025 <limogesa@outlook.com>
2025-05-09 03:41:13 -0400

    tests

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const { Builder, By, until } = require('selenium-webdriver');
-
+const assert = require("assert");
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get('http://localhost:4444/wd/hub');
         let title = await driver.getTitle();
+        assert.strictEqual(title, "Plant Calendar")
         console.log("Page title is:", title);
     } finally {
         await driver.quit();
