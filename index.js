d41fc813d9ce7040547875a58eb6e01660f8d7d7
Annette-2025 <limogesa@outlook.com>
2025-05-08 21:41:04 -0400

    'server'

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ app.get('/default/page2', (req, res) => {
     res.sendFile(__dirname + '/pages/calendar.html');
 })
 
+app.get('/default/february', (req, res) => {
+    res.sendFile(__dirname + '/pages/february.html');
+})
+
+app.get('/default/april', (req, res) => {
+    res.sendFile(__dirname + '/pages/april.html');
+})
+
+app.get('/default/may', (req, res) => {
+    res.sendFile(__dirname + '/pages/may.html');
+})
+
 app.post('/default/Plants', (req, res) => {
     res.sendFile(__dirname + '/pages/plantList.html');
     //const { side1, side2, side3 } = req.body;
@@ -23,51 +35,3 @@ app.post('/default/Plants', (req, res) => {
     //res.json({ result });
 })
 module.exports.handler = serverless(app);
-/*
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-const port = 1001
-
-http.createServer((req, res)=>{
-    var q = url.parse(req.url,true);
-    var filename = "."+q.pathname;
-    fs.readFile(filename, function(err,data){
-        if(err){
-            res.writeHead(404,{'Content-Type' : 'text/html'});
-            return res.end("Page not found");
-        }
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-    //res.writeHead(200,{'Content-Type': 'text/html'});
-    //var readStream = fs.createReadStream('public/plantList.html');
-    //readStream.pipe(res);
-}).listen(port);
-console.log("server running on port " + port + "\nAccess Webpage here: " + "http://localhost:" + port + "/pages/calendar.html");
-*/
-
-
-
-
-
-/*
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-
-app.get('/main', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-
-app.post('/main/result', (req, res) => {
-    
-})
-    */
